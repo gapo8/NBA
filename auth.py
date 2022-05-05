cbc822379da718c3a7b03181af68266d68102136
gapo8 <gasper1999@gmail.com>
2022-05-05 12:59:52 +0200

    dodal auth.py

---
+++ auth.py
@@ -0,0 +1,11 @@
+
+
+db = 'sem2022_gasperp'
+host = 'baza.fmf.uni-lj.si'
+user = 'gasper'
+password = 'gasper1234'
+
+db = 'sem2022_gasperp'
+host = 'baza.fmf.uni-lj.si'
+user = 'tine'
+password = 'tine1234'
